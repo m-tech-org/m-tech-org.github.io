a656d5852972ff5fcd22b0602e06040bf80fc557
m-tech-ltd <mtechltd2021@gmail.com>
2025-10-31 04:25:25 +0600

    add cors allowed host all *

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig(({mode}) => {
             host: env.VITE_HOST || '0.0.0.0',
             port: parseInt(env.VITE_PORT) || 5173,
             strictPort: true,
-            allowedHosts: []
+            allowedHosts: ['*']
         }
     };
 });
