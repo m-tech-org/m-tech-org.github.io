a898c2acbf05ac790cc6e389ec831a7747e14c8c
m-tech-ltd <mtechltd2021@gmail.com>
2025-11-03 02:54:03 +0600

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -26,8 +26,8 @@ export default defineConfig(({mode}) => {
         },
 
         server: {
-            // host: env.VITE_HOST || '0.0.0.0',
-            // port: parseInt(env.VITE_PORT) || 5173,
+            host: env.VITE_HOST || '0.0.0.0',
+            port: parseInt(env.VITE_PORT) || 5173,
             strictPort: true,
             allowedHosts: ['*']
         }
