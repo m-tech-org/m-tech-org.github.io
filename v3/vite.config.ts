45d137ffee30acba3e8626f5d3f5f5d4f4c9ac8f
m-tech-ltd <mtechltd2021@gmail.com>
2025-11-12 01:55:33 +0600

    update workflow and add build path

--- v3/vite.config.ts
+++ v3/vite.config.ts
@@ -1,16 +1,21 @@
 import {reactRouter} from "@react-router/dev/vite";
 import {defineConfig} from "vite";
-import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [react(), reactRouter(), tsconfigPaths()],
+    plugins: [
+        reactRouter(),
+        tsconfigPaths()
+    ],
     base: '/',
     build: {
         outDir: '../build',
         sourcemap: false,
     },
     server: {
-        port: 3000
+        port: 3000,
+        hmr: {
+            overlay: false
+        }
     }
 });
